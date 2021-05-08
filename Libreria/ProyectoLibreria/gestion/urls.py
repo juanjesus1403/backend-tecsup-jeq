5ae02ffbce1dac37f2eb4eaea60b2aa595a69f2d
Juan Jesus Enciso <juanjesus_eq96@hotmail.com>
2021-05-07 20:53:06 -0500

    limpieza de rama

--- Libreria/ProyectoLibreria/gestion/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import CRUDCategoriaController, ListarCategoriaController
-
-#! esta variable se tiene que llamar si o si asi:
-urlpatterns = [
-  path('categorias',ListarCategoriaController.as_view()),
-  path('categorias/<int:pk>', CRUDCategoriaController.as_view())
-]
