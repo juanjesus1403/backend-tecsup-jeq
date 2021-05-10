902ce8a9b80d32bbd8419c723fa45162d48eea32
Juan Jesus Enciso <juanjesus_eq96@hotmail.com>
2021-05-10 17:09:18 -0500

    limpieza de la rama

--- index.js
+++
@@ -1,3 +0,0 @@
-const {Server} = require("./config/Server")
-const objServer = new Server();
-objServer.iniciarServidor();
--- routes/tareas.js
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express'
-import {crearTarea, listarTareas, listarTareasPorId} from "../controllers/tareas"
-
-export const tareas_router = Router();
-
-tareas_router.route("/tareas").post(crearTarea).get(listarTareas);
-// para ahcer un parametro x url DINAMICO simplemente definimos el nombre de esa variable pero con ":" para que express sepa que sera dinamico 
-//
-tareas_router.route("/tareas/:id").get(listarTareasPorId)
